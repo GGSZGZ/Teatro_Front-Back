bdfa73d6e9993fdc0bc7dbfe12f2f303e0406637
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2023-12-04 17:15:11 +0100

    Carrusel

---
+++ scripts/carrusel.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll(".carrusel-slide");
+
+    function showSlide(index) {
+        slides.forEach((slide) => (slide.classList.remove("active")));
+        slides[index].classList.add("active");
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    // Mostrar el primer slide al cargar la página
+    showSlide(currentIndex);
+
+    // Event listeners para los botones de siguiente y anterior
+    document.getElementById("nextBtn").addEventListener("click", nextSlide);
+    document.getElementById("prevBtn").addEventListener("click", prevSlide);
+});
