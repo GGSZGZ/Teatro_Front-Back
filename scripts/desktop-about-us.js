6d3589917e6bf904663c8dd58c61f7069292a119
IvanSeguraRomero <a27312@svalero.com>
2023-12-05 19:34:37 +0100

    Form-Contactos

--- scripts/desktop-about-us.js
+++ scripts/desktop-about-us.js
@@ -18,3 +18,4 @@ addClickEvent("socialMediaContainer", "https://www.instagram.com/", true);
 addClickEvent("socialMediaIconSquare1", "https://twitter.com/", true);
 addClickEvent("homeText1", "./index.html", false);
 addClickEvent("contactText1", "./desktop-contactos.html", false);
+addClickEvent("send-message", "./index.html", false);
