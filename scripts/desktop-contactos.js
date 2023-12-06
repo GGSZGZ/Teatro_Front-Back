7494ef1e5c020b8f4d211b93c0cf5e48b95c904a
GGSZGZ <guillermo.garcia@fundacionmontessori.com>
2023-12-06 17:43:49 +0100

    Ajustes de merge corregidos

--- scripts/desktop-contactos.js
+++ scripts/desktop-contactos.js
@@ -23,3 +23,23 @@ addClickEvent("aboutText1", "./desktop-about-us.html", false);
 
 
 
+//evento boton para recogida de datos
+document.getElementById("send-message").addEventListener("click",()=>{
+  var name=document.getElementById("Nombre").value;
+  var mail=document.getElementById("Email").value;
+  var telephone=document.getElementById("Telephone").value;
+  var direction=document.getElementById("Location").value;
+  var message=document.getElementById("textarea").value;
+  if(name=="" || mail==""|| telephone==""||direction==""){
+    alert("Por favor rellene todos los campos obligatorios");
+  }else{
+    //mandar datos json
+    //vacio los campos del form
+    document.getElementById("formulario").reset();
+  }
+});
+
+
+
+
+
